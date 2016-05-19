f060196a7df2b7433105921963157d7f8687ed03
Camilo Reyes <reyesc@gmail.com>
2016-05-18 20:01:02 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+
+var app = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+
+  res.end('Hello, World!');
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,3 @@
+﻿#Developing An Isomorphic GraphQL App With React
+
+An overview of isomorphic applications with GraphQL and React
