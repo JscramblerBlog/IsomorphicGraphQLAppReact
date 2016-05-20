37f6524010dd172ab0616e5ccac967cd550896bb
Camilo Reyes <reyesc@gmail.com>
2016-05-19 19:28:27 -0500

    Add GraphQL API

---
+++ graphql/schema.js
@@ -0,0 +1,33 @@
+﻿var graphql = require('graphql');
+var data = require('./data.json');
+
+const drinkType = new graphql.GraphQLObjectType({
+  name: 'Drink',
+  fields: {
+    id: { type: new graphql.GraphQLNonNull(graphql.GraphQLInt) },
+    name: { type: graphql.GraphQLString },
+    count: { type: graphql.GraphQLInt }
+  }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    drink: {
+      type: drinkType,
+      args: {
+        id: { type: new graphql.GraphQLNonNull(graphql.GraphQLInt) }
+      },
+      resolve: function (root, args) {
+        return data[args.id];
+      }
+    }
+  }
+});
+
+const schema = new graphql.GraphQLSchema({
+  query: queryType,
+  types: [ drinkType ]
+});
+
+module.exports = schema;
---
+++ tests/graphql/schema.test.js
@@ -0,0 +1,21 @@
+﻿var schema = require('../../graphql/schema');
+var graphql = require('graphql');
+var should = require('should');
+
+describe('A bar tab schema', function () {
+  it('queries for a drink', function (done) {
+    const query = `
+      query GetDrinkTabQuery {
+        drink(id: 1) {
+          count
+        }
+      }
+    `;
+
+    graphql.graphql(schema, query).then(function (drinkTab) {
+      should(drinkTab.data.drink.count).be.above(0);
+
+      done();
+    });
+  });
+});
